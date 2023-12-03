02297e59906d040f2b02a8e2810749549e7357fb
Kendrick Mitchell <kenmitchell2012@icloud.com>
2023-12-02 23:56:12 -0600

    create accounts app

--- mystore/settings.py
+++ mystore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mystore.wsgi.application'
+
+
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 
 # Database
